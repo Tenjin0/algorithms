2dbedc4e30d35b2c453b60fcae88ae0ba72cb0ad
Tenji <patricepetit@hotmail.com>
2018-06-10 14:37:47 +0200

    Codewars: sort the odds.py

--- codewars/src/sort_the_odd.py
+++ codewars/src/sort_the_odd.py
@@ -1,15 +1,14 @@
 def sort_array(source_array):
-    print(source_array)
     for idx1, val1 in enumerate(source_array):
         if val1 % 2 == 1:
             min_indice_found = None
             min_val_found = val1
-            for idx2, val2 in enumerate(source_array[min(idx1 + 1,len(source_array)):]):
+            for idx2, val2 in enumerate(source_array[min(idx1 + 1,
+                                                         len(source_array)):]):
                 if val2 < min_val_found and val2 % 2 == 1:
                     min_val_found = val2
                     min_indice_found = idx1 + 1 + idx2
-            print(val1, min_indice_found, min_val_found)
-            # if min_indice_found is not None:
-            #     source_array[min_indice_found] = val1
-            #     source_array[idx1] = min_val_found
+            if min_indice_found is not None:
+                source_array[min_indice_found] = val1
+                source_array[idx1] = min_val_found
     return source_array
--- codewars/test/test_0.py
+++ codewars/test/test_0.py
@@ -1,12 +1,13 @@
 from src.sort_the_odd import sort_array
 import unittest
 
+
 class CodewarsTest(unittest.TestCase):
     def test_sort_the_odd(self):
         self.assertEqual(sort_array([5, 3, 2, 8, 1, 4]), [1, 3, 2, 8, 5, 4])
         self.assertEqual(sort_array([5, 3, 1, 8, 0]), [1, 3, 5, 8, 0])
-        self.assertEqual(sort_array([]),[])
+        self.assertEqual(sort_array([]), [])
+
 
 if __name__ == '__main__':
-    # unittest.main(verbosity=2)
-    print(sort_array([5, 3, 2, 8, 1, 4]))
+    unittest.main(verbosity=2)
